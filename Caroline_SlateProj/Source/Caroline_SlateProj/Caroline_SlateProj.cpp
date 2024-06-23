64b2a1238993493403ed58a6c6729bc08cf82e84
CarolineeeeeDev <LilScorpionC@gmail.com>
2024-06-23 16:48:25 +0800

    Revert "用Slate写控件布局"
    This reverts commit f6723a70f5e00d97810338cbe431c4b8cd8e9f22.

--- Caroline_SlateProj/Source/Caroline_SlateProj/Caroline_SlateProj.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Caroline_SlateProj.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Caroline_SlateProj, "Caroline_SlateProj" );
- 
--- Caroline_SlateProj/Source/Caroline_SlateProj/Private/Slate/MyGameMode.cpp
+++
@@ -1,9 +0,0 @@
-// Lehan Li's Slate Learning Project
-
-#include "Slate/MyGameMode.h"
-#include "Slate/MyHUD.h"
-
-AMyGameMode::AMyGameMode()
-{
-	HUDClass = AMyHUD::StaticClass();
-}
